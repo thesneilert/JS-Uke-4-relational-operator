b68ebe03ac271b080a890b4d8bca4c9f5233f501
thesneilert <122097231+thesneilert@users.noreply.github.com>
2023-01-30 14:10:53 +0100

    push

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const app = document.getElementById("app"); //henter div med id "app"
 
 let firstNumber;
 let secondNumber;
-let pointsNumber;
+let pointsNumber = 0;
 
 
 //view
 function updateView() {
     document.getElementById("app").innerHTML = `
-    Points: ${pointsNumber}
+    Points: <span style="color:#b58900;">${pointsNumber}</span>
     <br> <br>
     ${firstNumber}
-    <input type="text"> 
+    <input type="text" placeholder=" >  =  <"> 
     ${secondNumber}
     <br> <br>
     <button onclick="btnNewNumbers()">New</button>
@@ -23,27 +23,34 @@ function updateView() {
 
 
 //controller
-startGame(); //starter spill, i funksjonen er det updateView som starter html
+startGame(); //starter spill; i funksjonen er det updateView som starter html
 function startGame(){
     firstNumber = 1;
     secondNumber = 10;
     updateView(); //denne gjør at innerhtml vises
 }
 
+function showPointsNumber(){
+    app.innerHTML = pointsNumber;''
+}
+
 //random nummere funksjon
 function btnNewNumbers(){
-    firstNumber = Math.ceil(Math.random() * 10);
-    secondNumber = Math.ceil(Math.random() * 10);
+    firstNumber = Math.round(Math.random() * 10);
+    secondNumber = Math.round(Math.random() * 10);
     updateView();
 }
 
 //submit knappen
 function btnSubmit(){
-
+    showPointsNumber();
 }
 
 //reset knappen
 function btnReset(){
-
+    firstNumber = 1;
+    secondNumber = 10;
+    pointsNumber = 0;
+    updateView();
 }
 
